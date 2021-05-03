e8915cebabba0bb2b38dd06ca88b38d8edd4082c
billybutcher <34742984+billybutcher@users.noreply.github.com>
2021-05-03 19:12:23 +0800

    removed /

--- configgenerator.py
+++ configgenerator.py
@@ -3,7 +3,7 @@ import json
 jsondata = {
     "word2vecfile": "/content/GoogleNews-vectors-negative300.bin",
     "choidataset": "/content/text-segmentation/data/choi",
-    "wikidataset": "//content/wiki_727",
+    "wikidataset": "/content/wiki_727",
 }
 
 with open('config.json', 'w') as f:
